892b67f2ead4e0a1ea6d1d2b3cc054c8f791c85f
houdini473 <jacob@walkermarch.com>
2022-07-12 13:09:53 -0700

    show shortened month

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
-import { Dimensions } from 'react-native';
-import moment from 'moment';
+import { Dimensions } from "react-native";
+import moment from "moment";
 
-const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 export const CONTENT_OFFSET = 16;
 export const CONTAINER_HEIGHT = SCREEN_HEIGHT - 60;
-export const DATE_STR_FORMAT = 'YYYY-MM-DD';
+export const DATE_STR_FORMAT = "YYYY-MM-DD";
 export const availableNumberOfDays = [1, 3, 5, 7];
 
 const TIMES_WIDTH_PERCENTAGE = 18;
@@ -13,7 +13,7 @@ const PAGE_WIDTH_PERCENTAGE = (100 - TIMES_WIDTH_PERCENTAGE) / 100;
 export const computeWeekViewDimensions = (totalWidth, numberOfDays) => {
   // Each day must have an equal width (integer pixels)
   const dayWidth = Math.floor(
-    (totalWidth * PAGE_WIDTH_PERCENTAGE) / numberOfDays,
+    (totalWidth * PAGE_WIDTH_PERCENTAGE) / numberOfDays
   );
   const pageWidth = numberOfDays * dayWidth;
 
@@ -90,13 +90,13 @@ export const addLocale = (locale, obj) => {
 };
 
 export const getCurrentMonth = (date) => {
-  return moment(date).format('MMMM Y');
+  return moment(date).format("MMM Y");
 };
 
 export const calculateDaysArray = (date, numberOfDays, rightToLeft) => {
   const dates = [];
   for (let i = 0; i < numberOfDays; i += 1) {
-    const currentDate = moment(date).add(i, 'd');
+    const currentDate = moment(date).add(i, "d");
     dates.push(currentDate);
   }
   return rightToLeft ? dates.reverse() : dates;
